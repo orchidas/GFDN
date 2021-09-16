1c1d6763e6873c4f3c34d662b94e770f52cd75a6
Orchisama Das <orchi@ccrma.stanford.edu>
2021-09-17 00:25:06 +0100

    Updated DelayLine implementation

--- Source/DelayLine.cpp
+++ Source/DelayLine.cpp
@@ -10,23 +10,19 @@
 
 #include "DelayLine.h"
 
-//constructor and destructor
-DelayLine::DelayLine(){}
-DelayLine::~DelayLine(){
-    delete [] delayBuffer;
-}
 
+DelayLine::DelayLine(){}
+DelayLine::~DelayLine(){}
 
 //initialize delay line
 void DelayLine::prepare(const int L, const float sampleRate){
     
     length = L;  //length of delay line in samples
-    delayBuffer = new float[length];
-    for (int i = 0; i < L; i++)     //initializa delay lines to prevent garbage memory values
+    for (int i = 0; i < maxDelay; i++)     //initializa delay lines to prevent garbage memory values
         delayBuffer[i] = 0.0f;
     
     T60Filter.reset(sampleRate);     //filter at the end of the delay line
-
+    
 }
 
 // update filter coefficients when parameters are changed by user
@@ -34,14 +30,23 @@ void DelayLine::setFilterCoefficients(float gDC, float gPI, float fT){
     T60Filter.updateCoeff(gDC, gPI, fT);
 }
 
-// push at the start, pop from the end, shift all delay line elements to the right
-// finally, pass through T60 filter
-float DelayLine::update(const float input){
+float DelayLine::read(){
+ 
+    return T60Filter.process(delayBuffer[readPtr]);
+}
+
+void DelayLine::write(const float input){
+  
+    delayBuffer[writePtr] = input;
+}
+
+void DelayLine::update(){
+    --writePtr;
     
-    float output = delayBuffer[length-1];
-    for (int i = 0; i < length-1; i++){
-        delayBuffer[i+1] = delayBuffer[i];
-    }
-    delayBuffer[0] = input;
-    return T60Filter.process(output);
+    if (writePtr < 0) // wrap write pointer
+        writePtr = maxDelay - 1;
+    
+    readPtr = writePtr + length;
+    if (readPtr >= maxDelay) // wrap read pointer
+        readPtr -= maxDelay;
 }
--- Source/DelayLine.h
+++ Source/DelayLine.h
@@ -11,21 +11,25 @@
 #pragma once
 #include "ShelfFilter.h"
 
+//I referenced Jatin's FDN DelayUtils.h to update the DelayLine read and write pointers
+
 class DelayLine{
 public:
     DelayLine();
     ~DelayLine();
     
-    void setFilterCoefficients(float gDC, float gPI, float fT);
-    void prepare(const int L_ms, const float sampleRate);    //function to set delay line length
-    float update(const float input); // push and pop from delay line
-
+    void setFilterCoefficients(float gDC, float gPI, float fT);     //T60 filter at end of delayline
+    void prepare(const int L, const float sampleRate);    //function to set delay line length
+    float read();        //read from pointer
+    void write(const float input);      //write a pointer
+    void update();      //update Pointers
     
 private:
-    float* delayBuffer;
-    int length;
-    float sampleRate;
+    enum
+    {
+        maxDelay = 64*8192,
+    };
+    float delayBuffer[maxDelay];
+    int readPtr = 0, writePtr = 0, length;
     ShelfFilter T60Filter;
-    
-    
 };
